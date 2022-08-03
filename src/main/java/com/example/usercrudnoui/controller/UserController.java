61432ed042c738e77159ed40cdb158ca865a4da7
dylondi <dylon.diruscio@hotmail.com>
2022-08-03 17:45:33 +0200

    comments

--- src/main/java/com/example/usercrudnoui/controller/UserController.java
+++ src/main/java/com/example/usercrudnoui/controller/UserController.java
@@ -6,35 +6,67 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Processes incoming CRUD User REST requests.
+ */
 @RestController
 public class UserController {
 
+    /**
+     * Constructor Dependency Injection
+     */
     private final UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
+    /**
+     * Retrieves a List of all Users from UserService's listAll() method.
+     * @return List of All Users
+     */
     @GetMapping("/users")
     public List<User> findAllUsers(){
         return userService.listAll();
     }
 
+    /**
+     * Creates a new with UserService's createUser() method and the User
+     * Object passed in the RequestBody.
+     * @param user
+     * @return New User Object
+     */
     @PostMapping("/users/create")
     public User createNewUser(@RequestBody User user){
         return userService.createUser(user);
     }
 
+    /**
+     * Retrieves a User Object with specified id.
+     * @param id
+     * @return User Object
+     */
     @GetMapping("/users/user/{id}")
     public User findUserById(@PathVariable Integer id) {
         return userService.findUserById(id);
     }
 
+    /**
+     * Updates a User Object with UserService's updateUser() method and the
+     * User Object passed in the RequestBody.
+     * @param user
+     * @return Updated User Object
+     */
     @PutMapping("/users/update")
     public User updateUser(@RequestBody User user){
         return userService.updateUser(user);
     }
 
+    /**
+     * Deletes a User Object with specified id passed as method parameter.
+     * @param id
+     * @return String containing message
+     */
     @DeleteMapping("/users/delete/{id}")
     public String deleteUser(@PathVariable Integer id){
         return userService.deleteUserById(id);
--- src/main/java/com/example/usercrudnoui/service/UserService.java
+++ src/main/java/com/example/usercrudnoui/service/UserService.java
@@ -7,9 +7,15 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * Spring Service Class containing User CRUD operations' business logic.
+ */
 @Service
 public class UserService {
 
+    /**
+     * Constructor Dependency Injection
+     */
     final PasswordEncoder passwordEncoder;
     private final UserRepository repository;
 
@@ -18,19 +24,37 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    /**
+     * Retrieves list of all Users from database with UserRepository's finaAll() method.
+     * @return List of Users
+     */
     public List<User> listAll(){
         return repository.findAll();
     }
 
+    /**
+     * Saves a User Object to the database with UserRepository's save() method. The password
+     * associated with the User is encoded before saving.
+     */
     public User createUser(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         return repository.save(user);
     }
 
+    /**
+     * Retrieves a User object with specified id from database if exists with UserRepository's findById() method.
+     * @param id
+     * @return User object or null if not found
+     */
     public User findUserById(Integer id){
         return repository.findById(id).orElse(null);
     }
 
+    /**
+     * Deletes User Object with specified id from database if exists with UserRepository's deleteById() method.
+     * @param id
+     * @return String message.
+     */
     public String deleteUserById(Integer id) {
 
         repository.deleteById(id);
@@ -38,6 +62,11 @@ public class UserService {
     }
 
 
+    /**
+     * Updates an existing User Object with new values with UserRepository's save() method.
+     * @param user
+     * @return Updated User Object
+     */
     public User updateUser(User user){
         User existingUser = repository.findById(user.getId()).orElse(null);
         existingUser.setFullName(user.getFullName());
